2b2e8d2700ba274287105f99fc557e5056ba4f67
pchu22 <pmdc2001@gmail.com>
2024-04-21 20:33:01 +0100

    Remove Unused Exceptions.

--- E-learning_Application/src/com/es2/Exceptions/ObjectNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.es2.Exceptions;
-
-public class ObjectNotFoundException extends Exception{
-    public ObjectNotFoundException(String message){
-        super(message);
-    }
-}
--- E-learning_Application/src/com/es2/Exceptions/PoolExhaustedException.java
+++
@@ -1,7 +0,0 @@
-package com.es2.Exceptions;
-
-public class PoolExhaustedException extends Exception{
-    public PoolExhaustedException(String message){
-        super(message);
-    }
-}
